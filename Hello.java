1f5677d6af5796f77a73f451b6bb73dcba725889
Nikki Lin <nikkiylin@gmail.com>
2019-05-28 23:47:44 -0700

    added summer

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hellow new system!");
+        String s = "Summer";
+        System.out.println("Hellow new system! " + s);
+        
+
     }
+
+
+    
 }
